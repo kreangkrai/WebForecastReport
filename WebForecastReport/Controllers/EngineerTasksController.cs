3a51304cdd6e002f635477a719fd9764a30416c2
Black - BKP <black.bkp@gmail.com>
2022-03-15 16:36:51 +0700

    Add View Engineer Tasks

---
+++ WebForecastReport/Controllers/EngineerTasksController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebForecastReport.Interface;
+using WebForecastReport.Interface.MPR;
+using WebForecastReport.Interfaces.MPR;
+using WebForecastReport.Models;
+using WebForecastReport.Models.MPR;
+using WebForecastReport.Service;
+using WebForecastReport.Service.MPR;
+using WebForecastReport.Services.MPR;
+
+namespace WebForecastReport.Controllers
+{
+    public class EngineerTasksController : Controller
+    {
+        readonly IWorkingHours WorkingHoursService;
+        readonly IAccessory Accessory;
+        readonly IEngUser EngineerService;
+        readonly IJobResponsible JobResponsibleService;
+
+        public EngineerTasksController()
+        {
+            WorkingHoursService = new WorkingHoursService();
+            Accessory = new AccessoryService();
+            EngineerService = new EngUserService();
+            JobResponsibleService = new JobResponsibleService();
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("Login") != null)
+            {
+                string user = HttpContext.Session.GetString("userId");
+                List<UserModel> users = new List<UserModel>();
+                users = Accessory.getAllUser();
+                UserModel u = users.Where(w => w.fullname.ToLower() == user.ToLower()).Select(s => new UserModel { name = s.name, department = s.department, role = s.role, section = "Eng" }).FirstOrDefault();
+                HttpContext.Session.SetString("Role", u.role);
+                HttpContext.Session.SetString("Name", u.name);
+                HttpContext.Session.SetString("Department", u.department);
+                HttpContext.Session.SetString("Section", u.section);
+                return View(u);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        [HttpGet]
+        public List<string> GetDepartments()
+        {
+            List<string> departments = EngineerService.GetUsers().Select(s => s.department).Distinct().ToList();
+            return departments;
+        }
+
+        [HttpGet]
+        public List<EngUserModel> GetUsers()
+        {
+            List<EngUserModel> users = EngineerService.GetUsers().OrderBy(o => o.user_name).ToList();
+            return users;
+        }
+
+        [HttpGet]
+        public List<JobResponsibleModel> GetJobResponsibles(string user_id)
+        {
+            List<JobResponsibleModel> jrs = JobResponsibleService.GetJobResponsible(user_id);
+            return jrs;
+        }
+    }
+}
