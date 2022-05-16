06b78530db1abc2dbecc05e43a4f63ad785e0d9e
Black - BKP <black.bkp@gmail.com>
2022-05-16 16:01:21 +0700

    Add Model Form Overtime

---
+++ WebForecastReport/Models/MPR/Form_OvertimeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebForecastReport.Models.MPR
+{
+    public class Form_OvertimeModel
+    {
+        public string employee_name { get; set; }
+        public string department { get; set; }
+        public string phone_number { get; set; }
+        public string normal_start_time { get; set; }
+        public DateTime month { get; set; }
+        public List<WorkingHoursModel> working_hours { get; set; }
+        public string approval { get; set; }
+        public string inspector { get; set; }
+        public string approver { get; set; }
+    }
+}
