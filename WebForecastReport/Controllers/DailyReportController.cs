5192ca9bd843df8daa2e0f786f7d7da9fcbba9ef
Black - BKP <black.bkp@gmail.com>
2022-05-17 08:55:18 +0700

    Add Daily Report Module

---
+++ WebForecastReport/Controllers/DailyReportController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebForecastReport.Interface;
+using WebForecastReport.Interface.MPR;
+using WebForecastReport.Models;
+using WebForecastReport.Models.MPR;
+using WebForecastReport.Service;
+using WebForecastReport.Service.MPR;
+
+namespace WebForecastReport.Controllers
+{
+    public class DailyReportController : Controller
+    {
+        readonly IAccessory Accessory;
+        readonly IJobResponsible JRService;
+        readonly IDRService DRService;
+
+        public DailyReportController()
+        {
+            Accessory = new AccessoryService();
+            JRService = new JobResponsibleService();
+            DRService = new ENG_DailyReportService();
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("Login") != null)
+            {
+                string user = HttpContext.Session.GetString("userId");
+                List<UserModel> users = new List<UserModel>();
+                users = Accessory.getAllUser();
+                UserModel u = users.Where(w => w.fullname.ToLower() == user.ToLower()).Select(s => new UserModel { name = s.name, department = s.department, role = s.role, section = "Eng" }).FirstOrDefault();
+                HttpContext.Session.SetString("Role", u.role);
+                HttpContext.Session.SetString("Name", u.name);
+                HttpContext.Session.SetString("Department", u.department);
+                HttpContext.Session.SetString("Section", u.section);
+                return View(u);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+
+        [HttpGet]
+        public List<JobResponsibleModel> GetJobResponsible(string user_id)
+        {
+            List<JobResponsibleModel> jrs = JRService.GetJobResponsible(user_id);
+            return jrs;
+        }
+
+        [HttpGet]
+        public List<ENG_DailyReportModel> GetDailyReport(string user_id, string month, string job_id)
+        {
+            List<ENG_DailyReportModel> dlrs = DRService.GetDailyReport(user_id, month, job_id);
+            return dlrs;
+        }
+
+        [HttpPost]
+        public string AddDailyReport(string dr_str)
+        {
+            ENG_DailyReportModel dlr = JsonConvert.DeserializeObject<ENG_DailyReportModel>(dr_str);
+            var result = DRService.AddDailyReport(dlr);
+            return result;
+        }
+
+        [HttpPatch]
+        public string EditDailyReport(string dr_str)
+        {
+            ENG_DailyReportModel dlr = JsonConvert.DeserializeObject<ENG_DailyReportModel>(dr_str);
+            var result = DRService.EditDailyReport(dlr);
+            return result;
+        }
+    }
+}
---
+++ WebForecastReport/Interface/MPR/IDRService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebForecastReport.Models.MPR;
+
+namespace WebForecastReport.Interface.MPR
+{
+    interface IDRService
+    {
+        List<ENG_DailyReportModel> GetDailyReport(string user_id, string month, string job_id);
+        string AddDailyReport(ENG_DailyReportModel dlr);
+        string EditDailyReport(ENG_DailyReportModel dlr);
+    }
+}
