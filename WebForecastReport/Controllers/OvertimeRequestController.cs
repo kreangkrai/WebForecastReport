b83101cdfd3dae05a156d1162c0ce2ce8cfa32fe
Black - BKP <black.bkp@gmail.com>
2022-05-17 09:10:04 +0700

    Add Request Overtime Module

---
+++ WebForecastReport/Controllers/OvertimeRequestController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebForecastReport.Interface;
+using WebForecastReport.Models;
+using WebForecastReport.Service;
+
+namespace WebForecastReport.Controllers
+{
+    public class OvertimeRequestController : Controller
+    {
+        readonly IAccessory Accessory;
+
+        public OvertimeRequestController()
+        {
+            Accessory = new AccessoryService();
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("Login") != null)
+            {
+                string user = HttpContext.Session.GetString("userId");
+                List<UserModel> users = new List<UserModel>();
+                users = Accessory.getAllUser();
+                UserModel u = users.Where(w => w.fullname.ToLower() == user.ToLower()).Select(s => new UserModel { name = s.name, department = s.department, role = s.role, section = "Eng" }).FirstOrDefault();
+                HttpContext.Session.SetString("Role", u.role);
+                HttpContext.Session.SetString("Name", u.name);
+                HttpContext.Session.SetString("Department", u.department);
+                HttpContext.Session.SetString("Section", u.section);
+                return View(u);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+    }
+}
