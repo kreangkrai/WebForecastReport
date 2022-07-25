c39cadde137fc3d00303b5cb93625f2effaed22d
Black - BKP <black.bkp@gmail.com>
2022-07-25 15:56:25 +0700

    Add Weekly Summary Table

--- WebForecastReport/Controllers/WeeklySummaryController.cs
+++ WebForecastReport/Controllers/WeeklySummaryController.cs
@@ -50,29 +50,29 @@ namespace WebForecastReport.Controllers
         public JsonResult GetWorkingHours(string week)
         {
             List<WorkingHoursModel> whs = WorkingHours.GetWorkingHours(Convert.ToInt32(week.Split("-")[0]), Convert.ToInt32(week.Split("W")[1]));
-            string[] engineers = whs.Select(s => s.user_id).Distinct().ToArray();
-            string[] jobs = whs.Select(s => s.job_id).Distinct().ToArray();
-            string[] tasks = whs.Select(s => s.task_id).Distinct().ToArray();
+            string[] engineers = whs.OrderBy(o => o.user_id).Select(s => s.user_id).Distinct().ToArray();
+            string[] jobs = whs.OrderBy(o => o.job_id).Select(s => s.job_id).Distinct().ToArray();
+            string[] tasks = whs.OrderBy(o => o.task_id).Select(s => s.task_id).Distinct().ToArray();
             List<WeeklySummaryModel> weekly = new List<WeeklySummaryModel>();
-            for(int i = 0; i < engineers.Count(); i++)
+            for(int i = 0; i < tasks.Count(); i++)
             {
                 for(int j = 0;j<jobs.Count();j++)
                 {
-                    for(int k = 0;k<tasks.Count();k++)
+                    for(int k = 0;k<engineers.Count();k++)
                     {
-                        List<WorkingHoursModel> wh = whs.Where(w => w.user_id == engineers[i] && w.job_id == jobs[j] && w.task_id == tasks[k]).ToList();
+                        List<WorkingHoursModel> wh = whs.Where(w => w.task_id == tasks[i] && w.job_id == jobs[j] && w.user_id == engineers[k]).ToList();
                         int hours = 0;
                         if(wh.Count > 0)
                         {
                             hours = wh.Sum(s => Convert.ToInt32((s.stop_time - s.start_time).TotalHours));
                         }
                         weekly.Add(new WeeklySummaryModel{
-                            user_id = engineers[i],
-                            user_name = whs.Where(w => w.user_id == engineers[i]).Select(s => s.user_name).FirstOrDefault(),
+                            user_id = engineers[k],
+                            user_name = whs.Where(w => w.user_id == engineers[k]).Select(s => s.user_name).FirstOrDefault(),
                             job_id = jobs[j],
                             job_name = whs.Where(w => w.job_id == jobs[j]).Select(s => s.job_name).FirstOrDefault(),
-                            task_id = tasks[k],
-                            task_name = whs.Where(w => w.task_id == tasks[k]).Select(s => s.task_name).FirstOrDefault(),
+                            task_id = tasks[i],
+                            task_name = whs.Where(w => w.task_id == tasks[i]).Select(s => s.task_name).FirstOrDefault(),
                             week = Convert.ToInt32(week.Split("-")[0]),
                             year = Convert.ToInt32(week.Split("W")[1]),
                             hours = hours
