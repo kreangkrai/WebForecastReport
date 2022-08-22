2ed578368fc82fba476826266ec370eacdafbfcf
Black - BKP <black.bkp@gmail.com>
2022-08-22 08:52:41 +0700

    Change Session Name to Login_MES on Weekly Summary View(ENG)

--- WebForecastReport/Controllers/WeeklySummaryController.cs
+++ WebForecastReport/Controllers/WeeklySummaryController.cs
@@ -47,7 +47,7 @@ namespace WebForecastReport.Controllers
 
         public IActionResult EngineerWeeklySummary()
         {
-            if (HttpContext.Session.GetString("Login") != null)
+            if (HttpContext.Session.GetString("Login_MES") != null)
             {
                 string user = HttpContext.Session.GetString("userId");
                 List<UserModel> users = new List<UserModel>();
