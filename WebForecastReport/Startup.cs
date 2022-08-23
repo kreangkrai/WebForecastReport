cac7c57105f16892bb351d2727d338a4abf71855
kreangkrai <kreangkrai.ratt@gmail.com>
2022-08-23 13:36:39 +0700

    Add Cors

--- WebForecastReport/Startup.cs
+++ WebForecastReport/Startup.cs
@@ -15,6 +15,7 @@ namespace WebForecastReport
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,12 +26,23 @@ namespace WebForecastReport
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddCors(option =>
+            {
+                option.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+                  {
+                      policy.WithOrigins("http://ctracking.contrologic.co.th");
+
+                  });
+            });
+
             services.AddSession(option =>
             {
                 option.IdleTimeout = TimeSpan.FromMinutes(120);
@@ -58,6 +70,7 @@ namespace WebForecastReport
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseSession();
+            app.UseCors(MyAllowSpecificOrigins);
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
